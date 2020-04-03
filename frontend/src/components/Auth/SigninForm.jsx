96c7b364408584a2a88d519d40528972de37e1d3
노우제 <woojenoh@gmail.com>
2020-04-03 23:14:35 +0900

    [FE] Auth Validity Check

--- frontend/src/components/Auth/SigninForm.jsx
+++ frontend/src/components/Auth/SigninForm.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import useInput from '../../hooks/useInput';
 import './SigninForm.scss';
 
 function SigninForm() {
+  const [email, emailInput] = useInput({ type: 'text', placeholder: '이메일', required: true });
+  const [password, passwordInput] = useInput({ type: 'password', placeholder: '패스워드', required: true });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!(email && password)) {
+      alert('입력한 정보를 다시 확인하세요.');
+    }
+  }
+
   return (
     <div className="signin-form-wrapper">
-      <form className="signin-form">
+      <form className="signin-form" onSubmit={handleSubmit}>
         <div className="signin-form-inputs">
-          <input type="text" placeholder="이메일" required />
-          <input type="text" placeholder="패스워드" required />
+          {emailInput}
+          {passwordInput}
         </div>
         <div className="signin-form-buttons">
           <button className="green">로그인</button>
--- frontend/src/components/Auth/SignupForm.jsx
+++ frontend/src/components/Auth/SignupForm.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+import useInput from '../../hooks/useInput';
 import './SignupForm.scss';
 
 function SignupForm() {
+  const [email, emailInput] = useInput({ type: 'text', placeholder: '이메일', required: true })
+  const [password, passwordInput] = useInput({ type: 'password', placeholder: '패스워드(영문, 숫자 포함 6~12자)', required: true })
+  const [passwordConfirm, passwordConfirmInput] = useInput({ type: 'password', placeholder: '패스워드 확인', required: true })
+  const [nickName, nickNameInput] = useInput({ type: 'text', placeholder: '닉네임(특수문자 제외 2자 이상)', required: true })
+
+  const isEmailOk = email.match(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i);
+  const isPasswordOk = password.match(/^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{6,12}$/);
+  const isPasswordConfirmOk = password !== '' && password === passwordConfirm;
+  const isNickNameOk = nickName.match(/^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{2,20}$/);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!(isEmailOk && isPasswordOk && isPasswordConfirmOk && isNickNameOk)) {
+      alert('입력한 정보를 다시 확인하세요.');
+    }
+  }
+
   return (
     <div className="signup-form-wrapper">
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={handleSubmit}>
         <div className="signup-form-inputs">
-          <input type="text" placeholder="이메일" required />
-          <input type="text" placeholder="패스워드" required />
-          <input type="text" placeholder="패스워드 확인" required />
-          <input type="text" placeholder="닉네임" required />
+          <div className="input-wrapper">
+            {emailInput}
+            {isEmailOk && <span className="valid-check">✔</span>}
+          </div>
+          <div className="input-wrapper">
+            {passwordInput}
+            {isPasswordOk && <span className="valid-check">✔</span>}
+          </div>
+          <div className="input-wrapper">
+            {passwordConfirmInput}
+            {isPasswordConfirmOk && <span className="valid-check">✔</span>}
+          </div>
+          <div className="input-wrapper">
+            {nickNameInput}
+            {isNickNameOk && <span className="valid-check">✔</span>}
+          </div>
         </div>
         <div className="signup-form-buttons">
           <button className="green">회원가입</button>
