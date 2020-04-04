73b2d442d5d0c7aedab4842242ffd4d99704e8dd
노우제 <woojenoh@gmail.com>
2020-04-04 21:16:21 +0900

    [FE] lodash 설치 & isEqual 적용

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { isEqual } from 'lodash';
 import * as userActionCreators from '../../store/actionCreators/user';
 import "./Header.scss";
 
 function Header() {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.user.currentUser);
+  const user = useSelector(state => state.user.currentUser, isEqual);
 
   useEffect(() => {
     dispatch(userActionCreators.fetchCurrentUser());
